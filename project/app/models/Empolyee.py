c8267619bdf268c917a5d1f265c240e37f281352
Talha Akbar <akbartalha40@gmail.com>
2024-07-08 20:15:36 +0500

    migration done

--- project/app/models/Empolyee.py
+++ project/app/models/Empolyee.py
@@ -7,5 +7,5 @@ class Empolyee(db.Model):
     phone = db.Column(db.String(11),nullable=True)
     email = db.Column(db.String(50),nullable =True)
     address = db.Column(db.String(100),nullable =True)
-    department_id = db.Column(db.integer,db.ForeignKey('department.department_id'),nullable =False)
+    department_id = db.Column(db.integer,db.ForeignKey('department.department_id'),nullable = False)
     job_id = db.Column(db.integer,db.Foreignkey('job.job_id'),nullable = False)
